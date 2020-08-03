ae1054dac725003ee691d290f003a3121fe2fe40
Matt Mason <masonmj10@gmail.com>
2020-08-03 05:40:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,8 +8,16 @@ class Author
   def posts 
     Post.all.select do |post| post.author == self 
      end 
-end 
+  end 
   
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self 
+  end 
+  
+  def self.post_count
+    Post.all.length 
+  end 
   
   
   
