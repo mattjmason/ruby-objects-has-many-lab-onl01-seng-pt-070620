23b1658398fcc9f45c4778882252cd3a85c50389
Matt Mason <masonmj10@gmail.com>
2020-08-03 05:37:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,43 @@
 class Artist 
+  attr_accessor :name 
+  
+  @@song_count = 0 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def add_song(song)
+    song.artist = self 
+    
+  end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self 
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 end 
 
