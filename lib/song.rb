020a076a1e78b4ca970aa97f8e4769d612737a62
Matt Mason <masonmj10@gmail.com>
2020-08-03 05:42:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,27 @@
 class Song 
+  attr_accessor :name, :artist 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save 
+    
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def artist_name
+    if @artist = artist 
+      @artist.name
+    else nil 
+    end 
+  end 
   
   
   
