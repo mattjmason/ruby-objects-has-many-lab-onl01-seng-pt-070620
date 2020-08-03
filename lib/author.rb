b825efd6534ef7f5ff44cf2a8f1e78fcdc269b67
Matt Mason <masonmj10@gmail.com>
2020-08-03 05:39:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,18 @@
 class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def posts 
+    Post.all.select do |post| post.author == self 
+     end 
+end 
+  
+  
+  
+  
   
 end 
 
